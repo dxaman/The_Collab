d88f0448fa1192f0fb8a7ef8ab2fd6c22e56278f
dxaman <amandixit04@gmail.com>
2021-07-03 14:37:40 +0530

    m

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight , TattiBg} from './HeroElements'
+import { Button } from '../ButtonElements'
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
+  return (
+    <HeroContainer id='home'>
+   
+  
+      <HeroBg>
+      
+        
+        
+      </HeroBg>
+      <TattiBg src={require('../../images/photo.png')} />
+      <HeroContent>
+        <HeroH1> Public Policy And Opinion Cell</HeroH1>
+        <HeroP>*Info about PPOC here*<br></br>**Insert some more catchy line here**</HeroP>
+        <HeroBtnWrapper>
+          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'               smooth={true} duration={500} spy={true} exact='true' offset={-80}>
+            Explore {hover ? <ArrowForward /> : <ArrowRight/>}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default Hero
---
+++ src/components/Info/Data.jsx
@@ -0,0 +1,49 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: true,
+  lightText: false,
+  lighttextDesc: true,
+  topLine: 'PPOC Section 0',
+  headline: 'PPOC Opening Line',
+  description: 'PPOC DETAILS',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: '/images/svg-1.svg',
+  alt: 'car',
+  dark: true,
+  primary: true,
+  darkText: true
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: false,
+  lightText: false,
+  lighttextDesc: false,
+  topLine: 'PPOC Section1',
+  headline: 'PPOC Opening line',
+  description: 'PPOC DETAILS',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: '/images/svg-4.svg',
+  alt: 'Piggybanck',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: false,
+  lightText: false,
+  lighttextDesc: false,
+  topLine: 'PPOC Section 2',
+  headline: 'PPOC Opening Line',
+  description: 'PPOC DETAILS',
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: 'https://pbs.twimg.com/profile_images/1179368858769080320/K9Kx2P_w.jpg',
+  alt: 'car',
+  primary: false,
+  darkText: true
+};
