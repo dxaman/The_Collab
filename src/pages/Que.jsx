a76eb9e8ceb3699b8e2f10f8ad7be52ad10c5966
Satyam2201 <satyambirpur11@gmail.com>
2021-09-30 22:53:35 +0530

    page-added

---
+++ src/pages/Que.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import Que from '../components/Question/pages/index';
+
+const que_pages=()=> {
+  return (
+    <div className="App">
+      <Que/>
+    </div>
+  );
+}
+
+export default que_pages;
+
