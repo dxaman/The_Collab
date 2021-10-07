fc4a74ba3db472bd60a3dae673500b4a3da264b0
kkumar-gcc <84431594+kkumar-gcc@users.noreply.github.com>
2021-10-07 12:05:07 +0530

    footer add

--- src/components/Footer/FooterElements.jsx
+++ src/components/Footer/FooterElements.jsx
@@ -1,83 +1,22 @@
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const FooterContainer = styled.footer`
-  background-color: #101522;
-`
-
-export const FooterWrap = styled.div`
-  padding: 48px 24px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  max-width: 1100px;
-  margin: 0 auto;
-`
-export const FooterLinkContainer = styled.div`
-  display: flex;
-  justify-content: center;
-
-  @media screen and (max-width: 820px) {
-    padding-top: 32px;
-  }
-`
-
-export const FooterLinkWrapper = styled.div`
-  display: flex;
-
-  @media screen and (max-width: 820px) {
-    flex-direction: column;
-  }
-`
-export const FooterLinkItems = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin: 16px;
-  text-align: left;
-  width: 160px;
-  box-sizing: border-box;
-  color: #fff;
-
-  @media screen and (max-width: 420px) {
-    margin: 0;
-    padding: 10px;
-    width: 100%;
-  }
-`
-export const FooterLinkTitle = styled.h1`
-  font-size: 14px;
-  margin-bottom: 16px;
-`
-export const FooterLink = styled(Link)`
-  color: #fff;
-  text-decoration: none;
-  margin-bottom: 0.5rem;
-  font-size: 14px;
-
-  &:hover {
-    color: #01bf71;
-    transition: 0.3s ease-in-out;
-  }
-`
 export const SocialMedia = styled.section`
   max-width: 1000px;
   width: 100%;
-`
+`;
 
 export const SocialMediaWrap = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  position: relative;
   max-width: 1100px;
   margin: 40px auto 0 auto;
 
   @media screen and (max-width: 820px) {
     flex-direction: column;
   }
-`
+`;
 
 export const SocialLogo = styled(Link)`
   justify-self: start;
@@ -85,31 +24,39 @@ export const SocialLogo = styled(Link)`
   text-decoration: none;
   display: flex;
   align-items: center;
-`
+`;
 export const WebsiteRights = styled.small`
   color: #fff;
   margin-bottom: 16px;
-`
+`;
 export const SocialIcons = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 240px;
-`
+`;
 export const SocialIconLink = styled.a`
   color: #fff;
   font-size: 24px;
-`
-export const Contact = styled.text`
-  align-items: center;
+`;
+export const Tittle = styled.h1`
+  font-size: 24px;
+  line-height: 1.1;
+  text-align: left;
+  font-weight: 700;
   color: #fff;
-  font-size:13px;
-`
-export const Contactdiv = styled.div`
-  max-width: 1000px;
-  position: absolute;
-  top: -40%;
-  left: 750px;
-  width: 30%;
-  align-items: center;
-`
+  margin-top: 15px;
+
+  @media screen and (max-width: 700px) {
+    font-size: 25px;
+  }
+`;
+export const TopLine = styled.p`
+  color: #8e8e91;
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 700;
+  padding-top: 10px;
+  letter-spacing: 1.4px;
+  ${"" /* text-transform: uppercase; */}
+`;
--- src/components/igs/IGS_Footer/index.jsx
+++ src/components/igs/IGS_Footer/index.jsx
@@ -1,49 +1,145 @@
-import React from 'react'
-import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
-import { IGS_FooterContainer, IGS_FooterWrap, IGS_FooterLinkWrapper, IGS_FooterLinkItems, IGS_FooterLinkContainer, IGS_FooterLinkTitle, IGS_FooterLink, SocialIconLink, SocialIcons, SocialLogo, SocialMedia, SocialMediaWrap, WebsiteRights } from './IGS_FooterElements'
-import { animateScroll as scroll } from 'react-scroll';
-import {NavLogo} from "../IGS_Navbar/NavbarElements";
-
-const IGS_Footer = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+  FaPhoneSquareAlt,
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import {
+  TopLine,
+  Tittle,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+} from "./FooterElements";
+import { animateScroll as scroll } from "react-scroll";
+import { useEffect, useState } from "react";
+import "./styles4.css";
+const PC_Footer = () => {
   const toggleHome = () => {
     scroll.scrollToTop();
-  }
+  };
+  const [showButton, setShowButton] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  // This function will scroll the window to the top
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
   return (
-    <IGS_FooterContainer>
-      <IGS_FooterWrap>
-        <SocialMedia>
-          <SocialMediaWrap>
-            <SocialLogo to='/' onClick={toggleHome}>
-              <img src="https://i.ibb.co/ngkty7R/ppocbw.pnggg"  />
+    <div className="footer2">
+      <div className="footer-content">
+        <div className="footer-section contact">
+          <SocialLogo to="/" onClick={toggleHome}>
+            <img src="https://i.ibb.co/zxtxY7C/Group-63.png" title="Logo of Policy Conclave"/>
+          </SocialLogo>
+          <TopLine>
+            <span>Public Policy</span>
+            <br />
+            <span>& Opinion Cell</span>
+          </TopLine>
+          <Tittle> Follow Us </Tittle>
+          <SocialIcons title="follow us here">
+            <SocialIconLink
+              href="//www.facebook.com/sos.iitk"
+              target="_blank"
+              arial-label="Facebook"
+            >
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink
+              href="//www.instagram.com/ppoc_iitk/"
+              target="_blank"
+              arial-label="Instagram"
+              className="instagram"
+            >
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href="//www.youtube.com/channel/UCL6N8QXTgLIdj2SzxYhHrag/videos"
+              target="_blank"
+              arial-label="Youtube"
+              className="youtube"
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink
+              href="/"
+              target="_blank"
+              arial-label="Twitter"
+              className="twitter"
+            >
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink
+              href="//www.linkedin.com/company/sos-iitkanpur/"
+              target="_blank"
+              arial-label="Linkedin"
+              className="linkedin"
+            >
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </div>
+        <div className="footer-section links">
+          <Tittle> Quick Links </Tittle>
+          <ul>
+            <Link to="/" className="link">
+              <li>Home</li>
+            </Link>
+            <Link to="../../igs" className="link">
+              <li>Indian Governance Summit</li>
+            </Link>
+            <Link to="../pc" className="link">
+              <li>Policy Conclave</li>
+            </Link>
+            <Link to="../../igs" className="link">
+              <li>link4</li>
+            </Link>
+            <Link to="../../igs" className="link">
+              <li>link5</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="footer-section aboutus">
+          <Tittle>Contact Us</Tittle>
+          <TopLine>Post :-</TopLine>
+          <TopLine>Name</TopLine>
+          <p>
+            <FaPhoneSquareAlt></FaPhoneSquareAlt> 1234567890
+          </p>
+          <p>
+            <HiOutlineMail></HiOutlineMail> info@example.com
+          </p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        PPOC © {new Date().getFullYear()} All rights reserved.
+      </div>
+      {showButton && (
+        <button
+          title="Back to top"
+          onClick={scrollToTop}
+          className="back-to-top"
+        >
+          &#8679;
+        </button>
+      )}
+    </div>
+  );
+};
 
-    
-            </SocialLogo>
-            <WebsiteRights>
-              PPOC © {new Date().getFullYear()} All rights reserved.
-            </WebsiteRights>
-            <SocialIcons>
-              <SocialIconLink href='//www.facebook.com/sos.iitk' target='_blank' arial-label='Facebook'>
-                <FaFacebook />
-              </SocialIconLink>
-              <SocialIconLink href='//www.instagram.com/ppoc_iitk/' target='_blank' arial-label='Instagram'>
-                <FaInstagram/>
-              </SocialIconLink>
-              <SocialIconLink href='//www.youtube.com/channel/UCL6N8QXTgLIdj2SzxYhHrag/videos' target='_blank' arial-label='Youtube'>
-                <FaYoutube />
-              </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' arial-label='Twitter'>
-                <FaTwitter/>
-              </SocialIconLink>
-              <SocialIconLink href='//www.linkedin.com/company/sos-iitkanpur/' target='_blank' arial-label='Linkedin'>
-                <FaLinkedin />
-              </SocialIconLink>
-            </SocialIcons>
-          </SocialMediaWrap>
-        </SocialMedia>
-      </IGS_FooterWrap>
-    </IGS_FooterContainer>
-  )
-}
-
-export default IGS_Footer
+export default PC_Footer;
