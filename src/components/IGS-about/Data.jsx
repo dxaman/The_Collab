3434d34ced261311b2ed38cda936204ca5b2703c
tanishq67 <52757132+tanishq67@users.noreply.github.com>
2021-10-09 16:07:06 +0530

    Update Data.jsx

--- src/components/IGS-about/Data.jsx
+++ src/components/IGS-about/Data.jsx
@@ -3,7 +3,7 @@ export const AboutOne = {
     lightBg: true,
     lightText: false,
     lighttextDesc: true,
-    headline: 'About Us',
+    headline: 'About',
     description: 'Public Policy and Opinion Cell is based in IIT Kanpur that brings global events into valuable perspective. We aim to provide our members with first-class exposure to world leaders, policymakers, historians and economists, engaging students in discussions on issues of social, economic, political and historical significance and expose them to the challenges of policymaking, encouraging them to pursue careers in the field of public policy and governance. In an irreversibly changing society, policy design and its implementation has become even more challenging specially for a diverse nation like ours. Bringing in the young minds to tackle these critical problems is our resolute aim. \n',imgStart: false,
     img: 'https://i.ibb.co/qC6JrK6/golu-1.png',
     alt: 'Golu',
