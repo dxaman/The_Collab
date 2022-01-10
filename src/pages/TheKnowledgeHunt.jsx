da47a8a451f5b180858e096cd372509fbe478f2d
dxaman <amandixit04@gmail.com>
2022-01-11 04:27:31 +0530

    merged

--- src/pages/TheKnowledgeHunt.jsx
+++ src/pages/TheKnowledgeHunt.jsx
@@ -6,6 +6,7 @@ import IGS_Footer from "../components/igs/IGS_Footer";
 import Sidebar from "../components/SideBarIGS";
 import ConductionDetail from '../components/TheKnowledgeHunt/ConductionDetail/ConductionDetail';
 import Register from '../components/TheKnowledgeHunt/Register/Register';
+import Hunt from './Hunt'
 
 
 
@@ -16,6 +17,7 @@ const Home = () => {
         <>
             {/* <Sidebar isOpen={isOpen} toggle={toggle} /> */}
             {/* <NavBar toggle={toggle} /> */}
+            <Hunt/>
             <Register />
             <ConductionDetail />
             {/* <IGS_Footer /> */}
