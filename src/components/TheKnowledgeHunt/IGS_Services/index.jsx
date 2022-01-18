6ff66c3b6dfbc9fb79999b83b9edc01382a8c9e8
dxaman <amandixit04@gmail.com>
2022-01-18 12:32:17 +0530

    updated

--- src/components/TheKnowledgeHunt/IGS_Services/index.jsx
+++ src/components/TheKnowledgeHunt/IGS_Services/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Icon1 from '../../../images/aa.jpg'
 import Icon2 from '../../../images/nn.jpg'
+import Icon3 from '../../../images/pp.jpg'
 import { Button2 } from '../IGS_ButtonElements'
 import {IGS_ServicesContainer, IGS_ServicesH1, IGS_ServicesWrapper, IGS_ServicesCard, IGS_ServicesIcon, IGS_ServicesH2, IGS_ServicesP} from './IGS_ServiceElements'
 
@@ -11,26 +12,26 @@ const IGS_Services = () => {
       <IGS_ServicesWrapper>
         <IGS_ServicesCard>
           <IGS_ServicesIcon src={Icon1}/>
-          <IGS_ServicesH2>Aditya</IGS_ServicesH2>
-          <IGS_ServicesP>Overall Coordinator<br/>+91-9999999999</IGS_ServicesP>
+          <IGS_ServicesH2>Aditya K Rao</IGS_ServicesH2>
+          <IGS_ServicesP>Overall Coordinator<br/>+91-8826240462</IGS_ServicesP>
           
         </IGS_ServicesCard>
         <IGS_ServicesCard>
         <IGS_ServicesIcon src={Icon2}/>
-        <IGS_ServicesH2>Nikhil</IGS_ServicesH2>
-          <IGS_ServicesP>Overall Coordinator<br/>+91-9999999999</IGS_ServicesP>
+        <IGS_ServicesH2>Nikhil Kumar Singh</IGS_ServicesH2>
+          <IGS_ServicesP>Overall Coordinator<br/>+91-7488746500</IGS_ServicesP>
           
         </IGS_ServicesCard>
         <IGS_ServicesCard>
-          <IGS_ServicesIcon src={Icon1}/>
-          <IGS_ServicesH2>Aditya</IGS_ServicesH2>
-          <IGS_ServicesP>Overall Coordinator<br/>+91-9999999999</IGS_ServicesP>
+        <IGS_ServicesIcon src={'https://i.ibb.co/RhgKj1Q/Purushottam-Ojha-DESIGN.jpg'}/>
+          <IGS_ServicesH2>Purushottam Ojha</IGS_ServicesH2>
+          <IGS_ServicesP>Design<br/>+91-9334287809</IGS_ServicesP>
           
         </IGS_ServicesCard>
         <IGS_ServicesCard>
-          <IGS_ServicesIcon src={Icon2}/>
-          <IGS_ServicesH2>Nikhil</IGS_ServicesH2>
-          <IGS_ServicesP>Overall Coordinator<br/>+91-9999999999</IGS_ServicesP>
+          <IGS_ServicesIcon src={Icon3}/>
+          <IGS_ServicesH2>Aman Dixit</IGS_ServicesH2>
+          <IGS_ServicesP>Web<br/>+91-9111427403</IGS_ServicesP>
           
         </IGS_ServicesCard>
       </IGS_ServicesWrapper>
