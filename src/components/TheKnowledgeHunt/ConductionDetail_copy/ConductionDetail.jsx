29b3acd156dc5086d69733dd79c5a7db785c7dfc
dxaman <amandixit04@gmail.com>
2022-01-18 13:08:44 +0530

    updated

--- src/components/TheKnowledgeHunt/ConductionDetail_copy/ConductionDetail.jsx
+++ src/components/TheKnowledgeHunt/ConductionDetail_copy/ConductionDetail.jsx
@@ -19,31 +19,31 @@ const ConductionDetail = () => {
                 <Accordion.Item eventKey="0" className="active">
                     <Accordion.Header className={(currentActiveKey === "0") ? "active" : ""}>How to pay fees?</Accordion.Header>
                     <Accordion.Body>
-                        <h5>SBI collect. Go to https://www.onlinesbi.com/sbicollect/icollecthome.html</h5>
+                        <h5>SBI collect. Go to https://www.onlinesbi.com</h5>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1" className={currentActiveKey === "1" ? "active" : ""}>
                     <Accordion.Header>How to pay fees?</Accordion.Header>
                     <Accordion.Body>
-                        <h5>SBI collect. Go to https://www.onlinesbi.com/sbicollect/icollecthome.html</h5>
+                        <h5>SBI collect. Go to https://www.onlinesbi.com</h5>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2" className={currentActiveKey === "2" ? "active" : ""}>
                     <Accordion.Header>How to pay fees?</Accordion.Header>
                     <Accordion.Body>
-                        <h5>SBI collect. Go to https://www.onlinesbi.com/sbicollect/icollecthome.html</h5>
+                        <h5>SBI collect. Go to https://www.onlinesbi.com</h5>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3" className={currentActiveKey === "3" ? "active" : ""}>
                     <Accordion.Header>How to pay fees?</Accordion.Header>
                     <Accordion.Body>
-                        <h5>SBI collect. Go to https://www.onlinesbi.com/sbicollect/icollecthome.html</h5>
+                        <h5>SBI collect. Go to https://www.onlinesbi.com</h5>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="4" className={currentActiveKey === "4" ? "active" : ""}>
                     <Accordion.Header>How to pay fees?</Accordion.Header>
                     <Accordion.Body>
-                        <h5>SBI collect. Go to https://www.onlinesbi.com/sbicollect/icollecthome.html</h5>
+                        <h5>SBI collect. Go to https://www.onlinesbi.com</h5>
                     </Accordion.Body>
                 </Accordion.Item>
 
